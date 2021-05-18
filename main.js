a1effef25c742995653a6f09851c65e572d70915
ewaapawlos <82997933+ewaapawlos@users.noreply.github.com>
2021-05-18 20:01:08 +0200

    Zmiana 5
    Dodanie gmin za pomocą geoservera,

--- main.js
+++ main.js
@@ -4,4 +4,20 @@ var mymap = L.map('mymap',
     	
 var lyrOSM = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
     	mymap.addLayer(lyrOSM);
-  });
+  
+var lyrPRG = 
+  L.tileLayer.wms("http://localhost:8095/geoserver/projekt_PG/wms",
+        {
+        layers: "projekt_PG:Gminy", 
+        format: 'image/png', 
+        transparent : 'true', 
+        version : '1.1.1'
+        });
+    mymap.addLayer(lyrPRG);
+
+    L.control.scale({position:'bottomleft',imperial:false, maxWidth:200}).addTo(mymap);
+  
+  
+    });
+
+ 
--- map/main.js
+++ map/main.js
@@ -3,5 +3,20 @@ var mymap = L.map('mymap',
 {center: [52.1, 21.0], zoom: 10, zoomControl: false, attributionControl: false});
     	
 var lyrOSM = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
-    	mymap.addLayer(lyrOSM);
-  });
+    	// mymap.addLayer(lyrOSM);
+ 
+var lyrPRG = L.tileLayer.wms("http://localhost:8095/geoserver/projekt_PG/wms",
+        {
+        layers: "projekt_PG:Gminy", 
+        format: 'image/png', 
+        transparent : 'true', 
+        version : '1.1.1'
+        });
+    mymap.addLayer(lyrPRG);
+
+
+ 
+ 
+    });
+
+ 
