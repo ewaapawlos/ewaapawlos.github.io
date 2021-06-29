6f11eac0d0d22e74340c734d13bbba22d7c4fe08
ewaapawlos <82997933+ewaapawlos@users.noreply.github.com>
2021-06-29 16:43:38 +0200

    Poprawiony projekt
    Zmiana stylu tytułu

--- projekt/main_kontakt.js
+++ projekt/main_kontakt.js
@@ -7,10 +7,9 @@ mymap.addLayer(lyrOSM);
 // L.circleMarker([52.253191, 20.900147], {radius: 5, color:'#ff0000'}).addTo(mymap);
 
 var myIcon = L.icon({
-    iconUrl: 'img/markere.png',
-    iconSize: [20, 34],
-    iconAnchor: [10, 34],
-
-})
+  iconUrl: "img/markere.png",
+  iconSize: [20, 34],
+  iconAnchor: [10, 34],
+});
 
 L.marker([52.253191, 20.900147], { icon: myIcon }).addTo(mymap);
